c2b74cdfff017635a620874272b91ea59bd007c1
Ruthvik <ruthvikchowdary@csus.edu>
2018-12-04 20:15:54 -0800

    menu stuff

--- src/com/group5/MainWindow.java
+++ src/com/group5/MainWindow.java
@@ -1,22 +1,36 @@
-package com.group5;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class MainWindow extends JFrame
-{
-    public MainWindow()
-    {
-        super();
+public class MainWindow extends JFrame {
+	public MainWindow() {
+		super();
+		JMenuBar menuBar;
+		JMenu help, File;
+		JMenuItem Export;
+		JMenu Options_Sub;
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Options_Sub = new JMenu("Options");
+		Options_Sub.add(new JMenuItem("Documents"));
+		Options_Sub.add(new JMenuItem("Sources"));
+		menuBar = new JMenuBar();
+		File = new JMenu("File");
+		File.add(new JMenuItem("Load Query"));
+		File.addSeparator();
+		File.add(Options_Sub);
+		File.addSeparator();
+		Export = new JMenuItem("Export");
+		File.add(Export);
+		menuBar.add(File);
+		setJMenuBar(menuBar);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        setSize(800, 600);
+		setSize(800, 600);
 
-        Container contentPane = getContentPane();
+		Container contentPane = getContentPane();
 
-        TabView tabView = new TabView();
+		TabView tabView = new TabView();
 
-        contentPane.add(tabView);
-    }
+		contentPane.add(tabView);
+	}
 }
