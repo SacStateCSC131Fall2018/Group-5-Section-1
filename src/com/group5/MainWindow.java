da5adc36b07774319c6f9d149f3d8fd9b1037d71
dnr37 <daviibobo>
2018-12-06 15:55:38 -0800

    Resolving conflicts

--- src/com/group5/MainWindow.java
+++ src/com/group5/MainWindow.java
@@ -1,75 +1,4 @@
-<<<<<<< HEAD
-package com.group5;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
-public class MainWindow extends JFrame {
-	public MainWindow() {
-		super();
-		JMenuBar menuBar;
-		JMenu help, File;
-		JMenuItem Export;
-		JMenu Options_Sub;
-
-		Options_Sub = new JMenu("Options");
-		Options_Sub.add(new JMenuItem("Documents"));
-		Options_Sub.add(new JMenuItem("Sources"));
-		menuBar = new JMenuBar();
-		File = new JMenu("File");
-
-		File.add(new JMenuItem("Save Query"));
-		
-		File.add(new JMenuItem("Load Query"));
-		File.addSeparator();
-		File.add(Options_Sub);
-		File.addSeparator();
-		Export = new JMenuItem("Export");
-		File.add(Export);
-		menuBar.add(File);
-		
-		//HELP MENU ITEM
-		help = new JMenu("Help");
-		JMenuItem JAbout = new JMenuItem("About");
-		JAbout.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	JOptionPane.showMessageDialog(null, "PIREX - Made by Group 5: Daniel Kuzmin, David Liu, "
-        				+ "David Ray, Lonnie Spann Jr, Michael Dorst, Ruthvik Chowdary, Timothy Darrow.");
-
-            }
-
-		});
-		help.add(JAbout);
-		menuBar.add(help);
-		//END HELP MENU ITEM
-		
-		//EXIT MENU ITEM
-		File.addSeparator();
-		JMenuItem JExit = new JMenuItem("Exit");
-		File.add(JExit);
-		JExit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	System.exit(0);
-
-            }
-
-		});
-		//END EXIT MENU ITEM
-		
-		setJMenuBar(menuBar);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-		setSize(800, 600);
-
-		Container contentPane = getContentPane();
-
-		TabView tabView = new TabView();
-
-		contentPane.add(tabView);
-	}
-}
-
-=======
 package com.group5;
 import javax.swing.*;
 import java.awt.*;
@@ -135,4 +64,4 @@ public class MainWindow extends JFrame {
 		contentPane.add(tabView);
 	}
 }
->>>>>>> branch 'master' of https://github.com/SacStateCSC131Fall2018/Group-5-Section-1.git
+
